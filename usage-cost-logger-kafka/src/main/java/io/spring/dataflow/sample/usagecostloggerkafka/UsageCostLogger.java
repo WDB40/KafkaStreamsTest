3d6f322329e06cadde40442e0cf3261e98d26049
WDB40 <wbrown1640@gmail.com>
2020-01-07 22:00:13 -0600

    Initial commit

---
+++ usage-cost-logger-kafka/src/main/java/io/spring/dataflow/sample/usagecostloggerkafka/UsageCostLogger.java
@@ -0,0 +1,18 @@
+package io.spring.dataflow.sample.usagecostloggerkafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@EnableBinding(Sink.class)
+public class UsageCostLogger {
+
+    private static final Logger logger = LoggerFactory.getLogger(UsageCostLoggerKafkaApplication.class);
+
+    @StreamListener(Sink.INPUT)
+    public void process(UsageCostDetail usageCostDetail){
+        logger.info(usageCostDetail.toString());
+    }
+}
---
+++ usage-cost-processor-kafka/src/test/java/io/spring/dataflow/sample/usagecostprocessorkafka/UsageCostProcessorKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.dataflow.sample.usagecostprocessorkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageCostProcessorKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
